d890d30215866663af4c0f334a1b87c086c44c7d
Lucas Duarte <lucs.rkt@gmail.com>
2024-12-13 02:13:39 -0300

    feat: add simple controller

--- app/controllers/api/v1/redeem_pages_controller.rb
+++ app/controllers/api/v1/redeem_pages_controller.rb
@@ -3,7 +3,19 @@
 module Api
   module V1
     class RedeemPagesController < ApiController
-      def show; end
+      def show
+        redeem_page = RedeemPage.find_by(id: params[:id])
+
+        if redeem_page.nil?
+          render json: { error: 'Página de resgate não encontrada.' }, status: :not_found
+          return
+        elsif !redeem_page.is_active
+          render json: { error: 'Página de resgate não está ativa para novos resgates.' }, status: :unprocessable_entity
+          return
+        end
+
+        render json: redeem_page, status: :ok
+      end
     end
   end
 end
