b1f4ad49b8c2a1c914c3004a94b988e754a19252
Lucas Duarte <lucs.rkt@gmail.com>
2024-12-16 17:42:03 -0300

    test: add lasts tests

--- app/services/redeem_page_service.rb
+++ app/services/redeem_page_service.rb
@@ -3,7 +3,7 @@ class RedeemPageService
     redeem_page = RedeemPage.find_by(id: id)
 
     return { status: :not_found, error: 'Redeem page not found.' } if redeem_page.nil?
-    return { status: :unprocessable_entity, error: 'Redeem page is not active for new redeems.' } unless redeem_page.is_active
+    return { status: :forbidden, error: 'Redeem page is inactive.' } unless redeem_page.is_active
 
     { status: :ok, redeem_page: redeem_page }
   end
--- test/controllers/redeem_pages_controller_test.rb
+++ test/controllers/redeem_pages_controller_test.rb
@@ -3,33 +3,61 @@ require "test_helper"
 
 class RedeemPagesControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @redeem_page = RedeemPage.create
+    @redeem_page = RedeemPage.create!(
+      name: 'Test Page',
+      is_active: true,
+      size_options: ['Small', 'Medium', 'Large'],
+      extra_questions: [
+        { 'question' => 'What is your favorite color?', 'answer' => '' },
+        { 'question' => 'What is your pet’s name?', 'answer' => '' }
+      ]
+    )
   end
 
   describe 'GET /api/v1/redeem_pages/:id' do
     test 'should get redeem page data' do
       get api_v1_redeem_page_url(@redeem_page)
+
       assert_response :ok
+      json_response = JSON.parse(response.body)
+
+      assert_equal @redeem_page.id, json_response['id']
+      assert_equal @redeem_page.name, json_response['name']
+      assert_equal @redeem_page.is_active, json_response['is_active']
     end
 
     describe 'with sized products' do
       test 'should get redeem page data' do
         get api_v1_redeem_page_url(@redeem_page)
+
         assert_response :ok
+        json_response = JSON.parse(response.body)
+
+        assert_equal @redeem_page.size_options, json_response['size_options']
       end
     end
 
     describe 'with extra questions' do
       test 'should get redeem page data' do
         get api_v1_redeem_page_url(@redeem_page)
+
         assert_response :ok
+        json_response = JSON.parse(response.body)
+
+        assert_equal @redeem_page.extra_questions, json_response['extra_questions']
       end
     end
 
     describe 'inactive redeem page' do
       test 'should ommit redeem page data' do
+        @redeem_page.update(is_active: false)
+
         get api_v1_redeem_page_url(@redeem_page)
+
         assert_response :forbidden
+        json_response = JSON.parse(response.body)
+
+        assert_equal 'Redeem page is inactive.', json_response['error']
       end
     end
   end
